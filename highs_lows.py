31ff63fc4ad221baabc842a63098a2abc055390f
kam <kammif1990@gmail.com>
2017-03-27 14:49:33 +0500

    create minimum temperature analizing

--- highs_lows.py
+++ highs_lows.py
@@ -1,22 +1,30 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
   reader = csv.reader(f)
   header_row = next(reader)
-  highs = []
+  dates, highs, lows = [], [], []
   for row in reader:
-    high = int(row[1])
+    current_date = datetime.strptime(row[0], "%Y-%m-%d")
+    dates.append(current_date)
+    high = (row[1])
     highs.append(high)
+    low = int(row[3])
+    lows.append(low)
+
 
 #data input 
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c = 'red')
+plt.plot(dates, highs, c = 'red')
+plt.plot(dates, lows, c='blue')
 
 #Formatings diagram
-plt.title("Daily high temperatures, July 2014", fontsize = 24)
+plt.title("Daily high and low temperatures - 2014", fontsize = 24)
 plt.xlabel('', fontsize = 16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize = 16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
